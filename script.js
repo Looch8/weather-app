0bc93b3131ca4e734a19e8232edf026c0ddcf745
Luke Wheldale <lukewheldale@gmail.com>
2024-03-31 19:20:58 +1030

    feat: weather data extraction

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-console.log("hi");
-
-// Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
-
-// Function that takes location and return weather data
-
 // API key - 151ddc79dcaa4472ae173810243103
 
-console.log(
-	`https://api.weatherapi.com/v1/current.json?key=151ddc79dcaa4472ae173810243103&q=london`
-);
-
-// Get weather Data
+//Take location and return weather data
 async function getWeather(location) {
 	const response = await fetch(
-		`https://api.weatherapi.com/v1/current.json?key=151ddc79dcaa4472ae173810243103&q=${location}`
+		`https://api.weatherapi.com/v1/current.json?key=151ddc79dcaa4472ae173810243103&q=${location}`,
+		{ mode: "cors" }
 	);
-	const weatherData = await response.json();
+	weatherData = await response.json();
+
+	// call extractWeatherData to display specific data.
+	const extractedData = extractWeatherData(weatherData);
+
 	console.log(weatherData);
+	console.log(extractedData);
+
+	return weatherData;
 }
 
-getWeather(`london`);
-getWeather(`adelaide`);
+// Extract data from json data
+function extractWeatherData(weatherData) {
+	const weatherObj = {
+		city: weatherData.location.name,
+		country: weatherData.location.country,
+		temperature_celcius: `${weatherData.current.temp_c} celcius`,
+		temperature_fahrenheit: `${weatherData.current.temp_f} fahrenheit`,
+	};
+	return weatherObj;
+}
+getWeather("london");
+getWeather("adelaide");
+
+// Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
